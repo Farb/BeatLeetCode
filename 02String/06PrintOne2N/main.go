27667359cedbdef6b2103ae576a4d69747691e0b
farb <farbguo@qq.com>
2021-10-01 13:07:32 +0800

    add print 1 to N

---
+++ 02String/06PrintOne2N/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+//https://leetcode-cn.com/problems/da-yin-cong-1dao-zui-da-de-nwei-shu-lcof/
+
+func printNumbers(n int) []int {
+	if n < 0 {
+		return []int{}
+	}
+	max := int(math.Pow10(n))
+	res := make([]int, 0, max-n)
+	for i := 1; i < max; i++ {
+		res = append(res, i)
+	}
+	return res
+}
+
+func main() {
+	n := 0
+	fmt.Println(printNumbers(n))
+
+	n = 1
+	fmt.Println(printNumbers(n))
+
+	n = 2
+	fmt.Println(printNumbers(n))
+}
