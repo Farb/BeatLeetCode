6b4fcd96b2ed5baef68567dd04aaf6ed3dbfddbc
farb <farbguo@qq.com>
2025-03-31 10:48:31 +0800

    add leetcodeInCSharp P2278

---
+++ LeetCodeInCSharp/P2278.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCodeInCSharp;
+
+/// <summary>
+/// https://leetcode.cn/problems/percentage-of-letter-in-string/description/?envType=daily-question&envId=2025-03-31
+/// </summary>
+internal class P2278
+{
+    public int PercentageLetter(string s, char letter)
+    {
+        int letterCount = 0;
+        foreach (char c in s)
+        {
+            if (char.IsLetter(c))
+            {
+                letterCount++;
+            }
+        }
+        return letterCount * 100 / s.Length;
+    }
+}
